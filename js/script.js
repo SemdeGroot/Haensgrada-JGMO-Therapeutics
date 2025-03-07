0a0511e3e264466d9d4adc6dacc72875b5d99b2b
Sem de Groot <semdegroot2003@gmail.com>
2025-03-07 11:07:26 +0100

    Haensgrada by JGMO Therapeutics

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-document.getElementById("changeText").addEventListener("click", function() {
-    document.getElementById("message").innerText = "Hello World!";
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name && email && message) {
+        document.getElementById("formMessage").innerText = "Thank you! We will get back to you soon.";
+        document.getElementById("formMessage").style.color = "green";
+        this.reset();
+    } else {
+        document.getElementById("formMessage").innerText = "Please fill out all fields.";
+        document.getElementById("formMessage").style.color = "red";
+    }
 });
